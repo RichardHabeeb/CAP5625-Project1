5bddf2b72656bec2b8d04726d66489d35d169c7a
anwesht <anwesh.tuladhar@gmail.com>
2017-10-09 20:33:31 -0400

    Reading files.

--- app/Parser.js
+++ app/Parser.js
@@ -1,13 +1,35 @@
 export default (function() {
 
-    var Parser = function() {
-
-    }
-
-    Parser.prototype.getCities = function() {
-
+    var Parser = function(file) {
+        this.file = file;
     };
 
+    Parser.prototype.file = null;
+
+    Parser.prototype.getCities = function() {
+        var reader = new FileReader();
+        reader.onload = function(){
+            var lines = this.result.split('\n');
+
+            var outputElement = document.getElementById("output");
+            var listElement = document.createElement("ol");
+
+            outputElement.appendChild(listElement);
+
+            for(var lineNum = 0; lineNum < lines.length; lineNum++){
+                var line = lines[lineNum];
+
+                if (line === "END"){
+                    break
+                }
+
+                var li = document.createElement("li");
+                li.innerHTML = line;
+                listElement.appendChild(li)
+            }
+        };
+        reader.readAsText(this.file);
+    };
 
     return Parser;
 })();
